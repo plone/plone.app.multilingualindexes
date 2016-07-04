613974ca595ceb5598ded0518969a67de8805e1d
Patrick Gerken <patrick.gerken@zumtobelgroup.com>
2016-07-05 00:13:41 +0200

    Add fallback index
    @jensens, please check my comments.
    I am not sure if it makes sense to name that index Language2, but I also
    fear lots of issues if we replace Language with this index.
    I am not confident that I found all possible edge cases, the problem
    here is also, if something is a real edge case or if it can't ever happen.

--- src/plone/app/multilingualindexes/languagefallback.py
+++ src/plone/app/multilingualindexes/languagefallback.py
@@ -3,22 +3,28 @@ from App.class_init import InitializeClass
 from App.special_dtml import DTMLFile
 from logging import getLogger
 from plone.app.multilingual.interfaces import ITranslationManager
+from Products.CMFPlone.utils import safe_hasattr
+from Products.DateRecurringIndex.index import DateRecurringIndex
 from Products.PluginIndexes.common.UnIndex import UnIndex
-from Products.PluginIndexes.common.util import parseIndexRequest
 from Products.ZCatalog.Catalog import Catalog
+from ZODB.POSException import ConflictError
 
 
 logger = getLogger(__name__)
 _marker = object()
 
 
+CONFIG = {}
+
+
 def get_configuration():
-    return {
-        'de': ['en', 'it'],
-        'it': ['en', 'de'],
-        'en': ['de', 'it'],
-        'zh': ['en'],
-    }
+    global CONFIG
+    return CONFIG
+
+
+def set_configuration(config):
+    global CONFIG
+    CONFIG = config
 
 
 class LanguageFallbackIndex(UnIndex):
@@ -26,6 +32,7 @@ class LanguageFallbackIndex(UnIndex):
     """
 
     meta_type = 'LanguageFallbackIndex'
+    query_options = []
 
     def __init__(self, id, ignore_ex=None, call_methods=None,
                  extra=None, caller=None):
@@ -36,65 +43,78 @@ class LanguageFallbackIndex(UnIndex):
     @property
     def _catalog(self):
         # attention: we're using heuristics here
-        if hasattr(self.caller, 'uids') or isinstance(self.caller, Catalog):
+        if safe_hasattr(self.caller, 'uids'):
             return self.caller
-        elif hasattr(self.caller, '_catalog'):
+        elif isinstance(self.caller, Catalog):
+            return self.caller
+        elif safe_hasattr(self.caller, '_catalog'):
             return self.caller._catalog
         else:
             raise AttributeError(
                 'LanguageFallbackIndex cant work w/o knowing about its catalog'
             )
 
-    def _iterate_translation_fallbacks(self, context, callback):
-        status = 0
-        tm = ITranslationManager(context, None)
-        if not tm:
-            # looks like this one is not translatable
-            return status
-        translated = tm.get_translated_languages()
-        for main_lang, fallbacks in get_configuration().items():
-            found = None
-            for cur_lang in [main_lang] + fallbacks:
-                if cur_lang in translated:
-                    found = cur_lang
-            if not found:
-                continue
-            lang_obj = tm.get_translation(found)
-            uid_path = '/'.join(lang_obj.getPhysicalPath())
-            lang_doc_id = self._catalog.uids[uid_path]
-            callback(main_lang, lang_doc_id)
+    def getLangsIFallbackFor(self, lang):
+        for primary_lang, fallbacks in get_configuration().items():
+            if lang in fallbacks:
+                yield primary_lang
 
     def index_object(self, documentId, obj, threshold=None):
-        """index an object, normalizing the indexed value to an integer
-        """
-
-        def indexer_cb(main_lang, lang_doc_id):
-            # the unindex contains the information which doc_id was found the
-            # last time for the given language
-            old_lang_doc_id = self._unindex.get(
-                (documentId, main_lang),
-                _marker
-            )
-            if old_lang_doc_id == lang_doc_id:
-                return 0
-
-            self._unindex[(documentId, main_lang)] = lang_doc_id
-            self.removeForwardIndexEntry(main_lang, old_lang_doc_id)
-            self.insertForwardIndexEntry(main_lang, lang_doc_id)
-            return 1
-
-        return self._iterate_translation_fallbacks(obj, indexer_cb)
+        res = 0
+        obj_lang = obj.Language
+        old_obj_langs = self._unindex.get(documentId, [])
+        if obj_lang not in old_obj_langs:
+            if old_obj_langs is not []:
+                for old_lang in old_obj_langs:
+                    self.removeForwardIndexEntry(old_lang, documentId)
+                if obj_lang is _marker:
+                    try:
+                        for old_lang in old_obj_langs:
+                            self._unindex[documentId].pop(old_lang)
+                        if len(self._unindex[documentId]):
+                            del self._unindex[documentId]
+                    except ConflictError:
+                        raise
+                    except Exception:
+                        logger.exception('Should not happen: '
+                                         'old_obj_lang was there, now '
+                                         'it is not, for document: '
+                                         '%r', documentId)
+            if obj_lang is not _marker:
+                self.insertForwardIndexEntry(obj_lang, documentId)
+                self._unindex[documentId] = [obj_lang]
+            res = 1
+        if not obj_lang:
+            return res
+        wrapped_obj = obj._getWrappedObject()
+        translated_langs = set(ITranslationManager(wrapped_obj)
+                               .get_translations().keys()) - {obj_lang}
+        fallbacks = set(self.getLangsIFallbackFor(obj.Language))
+        for lang in fallbacks - translated_langs:
+            self.insertForwardIndexEntry(lang, documentId)
+            self._unindex[documentId].append(lang)
+            res = True
+        for lang in fallbacks & translated_langs:
+            if lang not in self._unindex[documentId]:
+                continue
+            self._unindex[documentId].remove(lang)
+            self.removeForwardIndexEntry(lang, documentId)
+        return res
 
     def unindex_object(self, documentId):
-        """ Carefully unindex the object with integer id 'documentId'
-        """
-        metadata = self._catalog.data[documentId]
+        unindexRecord = self._unindex.get(documentId, [])
+        if unindexRecord is []:
+            return None
 
-        def unindexer_cb(main_lang, lang_doc_id):
-            pass
-
-        return self._iterate_translation_fallbacks(obj, unindexer_cb)
-
+        for record in unindexRecord:
+            self.removeForwardIndexEntry(record, documentId)
+        try:
+            del self._unindex[documentId]
+        except ConflictError:
+            raise
+        except Exception:
+            logger.debug('Attempt to unindex nonexistent document'
+                         ' with id %r', documentId, exc_info=True)
 
 manage_addDRIndexForm = DTMLFile('www/addDRIndex', globals())
 
--- src/plone/app/multilingualindexes/tests/test_languagefallback.py
+++ src/plone/app/multilingualindexes/tests/test_languagefallback.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from plone.app.multilingual.interfaces import IPloneAppMultilingualInstalled
+from plone import api
+from plone.app.multilingual.interfaces import ITranslationManager
+from plone.app.multilingualindexes.languagefallback import set_configuration
 from plone.app.multilingualindexes.testing import PAMI_FUNCTIONAL_TESTING
-from plone.dexterity.utils import createContentInContainer
-from zope.interface import alsoProvides
 
 import unittest
 
@@ -15,3 +15,121 @@ class TestLFB(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
+        self.index = self.portal.portal_catalog.Indexes['Language2']
+        set_configuration(dict(ca=[], en=[], es=[]))
+
+    def make_obj(self, lang, title='Test'):
+        doc = api.content.create(
+            container=self.portal[lang],
+            type='Document',
+            title=title)
+        # XX We do a reindex here because an event handler sets Language
+        # too late. We could copy that event handler though
+        self.portal.portal_catalog.reindexObject(doc)
+        return doc
+
+    def search(self, lang):
+        return [x.getPath() for x in api.content.find(context=self.portal,
+                                                      SearchableText='Test',
+                                                      Language2=lang)]
+
+    def test_case_1(self):
+        """
+        No fallbacks
+        """
+        self.make_obj('ca')
+        self.assertEqual(1, len(self.search('ca')))
+        self.assertEqual(0, len(self.search('es')))
+        self.assertEqual(0, len(self.search('en')))
+
+    def test_case_2(self):
+        """
+        1 Fallback
+        """
+        set_configuration(dict(ca=['en'], en=[], es=[]))
+        self.make_obj('en')
+        self.assertEqual(1, len(self.search('ca')))
+
+    def test_case_3(self):
+        """
+        Documents are same but not identified as same
+        """
+        set_configuration(dict(ca=['en'], en=[], es=[]))
+        self.make_obj('en')
+        self.make_obj('ca')
+        self.assertEqual(2, len(self.search('ca')))
+        self.assertEqual(1, len(self.search('en')))
+
+    def test_case_4(self):
+        """
+        Documents are same
+        """
+        set_configuration(dict(ca=['en'], en=[], es=[]))
+        en_obj = self.make_obj('en')
+        ca_obj = self.make_obj('ca')
+        ITranslationManager(ca_obj).register_translation('en', en_obj)
+        self.assertEqual(1, len(self.search('ca')))
+        self.assertEqual(1, len(self.search('en')))
+
+    def test_case_5(self):
+        """
+        Documents become the same, later
+        """
+        set_configuration(dict(ca=['en'], en=[], es=[]))
+        en_obj = self.make_obj('en')
+        ca_obj = self.make_obj('ca')
+        self.assertEqual(2, len(self.search('ca')))
+        self.assertEqual(1, len(self.search('en')))
+        ITranslationManager(ca_obj).register_translation('en', en_obj)
+        self.assertEqual(1, len(self.search('ca')))
+        self.assertEqual(1, len(self.search('en')))
+
+    def test_case_6(self):
+        """
+        Do we leave no residue in index on deletion
+        """
+        set_configuration(dict(ca=['en'], en=[], es=[]))
+        # english portal gets reindexed on object creation
+        # so we add it to the empty index too
+        self.portal.portal_catalog.reindexObject(self.portal.en)
+        index = dict(self.index._index)
+        unindex = dict(self.index._unindex)
+        doc = self.make_obj('en')
+        self.assertEqual(1, len(self.search('ca')))
+        api.content.delete(doc)
+        self.assertEqual(0, len(self.search('ca')))
+        self.assertEqual(index, dict(self.index._index))
+        self.assertEqual(unindex, dict(self.index._unindex))
+
+    def test_case_7(self):
+        """
+        What is the desired behavior with two fallbacks
+        """
+        set_configuration(dict(ca=['en', 'es'], en=[], es=[]))
+        en_obj = self.make_obj('en')
+        self.make_obj('es')
+        self.assertEqual(2, len(self.search('ca')))
+        # self.assertEqual(('???', 'en'), self.search('ca'))
+        # If the result should be 1 here, we would have to do more extensive
+        # checks during index. For each language, we must check, if there are
+        # other languages of higher priority. Then we must check the TG
+        # to see, if any of these languages exists.
+        # On unindex, we must check, if any TG with lower prio exists.
+        api.content.delete(en_obj)
+        # self.assertEqual(('???', 'es'), self.search('ca'))
+        self.assertEqual(1, len(self.search('ca')))
+
+    def test_case_8(self):
+        """
+        No fallback needed any more
+        """
+        set_configuration(dict(ca=['en'], en=[], es=[]))
+        en_obj = self.make_obj('en')
+        self.assertEqual(1, len(self.search('ca')))
+        ca_obj = self.make_obj('ca')
+        # I think it is a bug that register Translation does not index
+        # both objects.
+        ITranslationManager(en_obj).register_translation('ca', ca_obj)
+        self.portal.portal_catalog.reindexObject(ca_obj)
+        self.portal.portal_catalog.reindexObject(en_obj)
+        self.assertEqual(['/plone/ca/test'], self.search('ca'))
