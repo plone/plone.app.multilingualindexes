0215f3d4025853cbc64d3cfaaf398ef6360679d9
Jens W. Klein <jk@kleinundpartner.at>
2016-10-15 08:40:29 -0400

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='plone.app.multilingualindexes',
-    version='1.0',
+    version='1.1.dev0',
     description="Multilingual Catalog Indexes for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
