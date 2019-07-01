2b91a90603a93a24c5d714e9f037ccb70195d39f
Jens W. Klein <jk@kleinundpartner.at>
2019-07-01 10:58:48 +0200

    py3: setup

--- setup.py
+++ setup.py
@@ -21,9 +21,13 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: Addon",
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -42,17 +46,10 @@ setup(
         'Products.CMFPlone',
         'Products.GenericSetup>=1.8.2',
         'setuptools',
-        'z3c.jbot',
     ],
     extras_require={
         'test': [
             'plone.app.testing',
-            # Plone KGS does not use this version, because it would break
-            # Remove if your package shall be part of coredev.
-            # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
             'plone.app.multilingual[test]',
             'plone.app.querystring >1.3.999'
         ],
